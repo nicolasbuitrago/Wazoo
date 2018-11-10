d6918b661cef3bf0f1323cb4b7aaa5901a475442
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2018-11-10 13:32:25 -0500

    Modified NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,8 +7,17 @@ class navBar extends React.Component {
   render() {
     return (
     <div className='navBar'>
+      <div className="home">
         <img src="imgs/wazoo.png" alt="Wazoo" height="50" width="50"/>
         <h1>Wazoo</h1>
+      </div>
+      <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li id="login"><a href="#login">Login</a></li>
+      </ul>
     </div>
     );
   }
