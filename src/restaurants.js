dffd277a78f30cf3489b64677276554276fc093b
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2018-11-09 19:53:52 -0500

    Added more restaurants

--- src/restaurants.js
+++ src/restaurants.js
@@ -7,7 +7,7 @@ var restaurants = {
           "name": "Los Compadres",
           "phoneFormatted": "(202) 234-7336",
           "phone": "2022347336",
-          "address": "1471 P St NW",
+          "address": "Cl. 74 # 46-96, Barranquilla, Atlántico",
           "city": "Barranquilla",
           "country": "Colombia",
           "crossStreet": "at 15th St NW",
@@ -42,6 +42,46 @@ var restaurants = {
           "type": "Point"
         },
         "id": "69ae4beb54d1d79b369551ba3b17737b"
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            -74.82906830635048,
+            11.005906311438395
+          ],
+          "type": "Point"
+        },
+        "type": "Feature",
+        "properties": {
+          "name": "La Traviata",
+          "phoneFormatted": "(202) 234-7336",
+          "phone": "2022347336",
+          "address": "Cl. 96 #46 - 32, Barranquilla, Atlántico",
+          "city": "Barranquilla",
+          "country": "Colombia",
+          "crossStreet": "at 15th St NW",
+          "state": "Atlantico"
+        }
+      },
+      {
+        "geometry": {
+          "coordinates": [
+            -74.79646054287987,
+            10.985662943272544
+          ],
+          "type": "Point"
+        },
+        "type": "Feature",
+        "properties": {
+          "name": "La Cueva",
+          "phoneFormatted": "(202) 234-7336",
+          "phone": "2022347336",
+          "address": "Cra. 43 #59-03, Barranquilla, Atlántico",
+          "city": "Barranquilla",
+          "country": "Colombia",
+          "crossStreet": "at 15th St NW",
+          "state": "Atlantico"
+        }
       }
     ]
   }
