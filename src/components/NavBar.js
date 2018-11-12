a6b4220cb1246b44c1d919f57ed7bef68d0c4499
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2018-11-12 11:31:50 -0500

    Working in routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,44 @@
 import React from 'react'
 
-import './NavBar.css'; // Imported css
+//import './NavBar.css'; // Imported css
 
 class navBar extends React.Component {
 
   render() {
     return (
-    <div className='navBar'>
-      <div className="home">
-        <img src="imgs/wazoo.png" alt="Wazoo" height="50" width="50"/>
-        <h1>Wazoo</h1>
-      </div>
-      <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-        <li id="login"><a href="#login">Login</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      Wazoo
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
       </ul>
     </div>
+  </nav>
     );
   }
 }
