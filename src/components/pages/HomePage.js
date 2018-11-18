e31b5dd786b96f360fa2cf5c16248b44ce14153d
John <jjcerpa@uninorte.edu.co>
2018-11-18 00:16:22 -0500

    New design for the home page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,30 +1,198 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { Button } from "semantic-ui-react";
-import PropTypes from 'prop-types';
-import * as actions from '../../actions/auth';
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react'
 
-const HomePage = ({ isAuthenticated, logout }) => (
-  <div>
-    <h1>Home Page</h1>
-    {isAuthenticated? (
-      <Button basic color='red' onClick={() => logout()}>Logout</Button> 
-    ) : (
-      <div><Link to='/login'>Login</Link> or <Link to='/signup'>Sign Up</Link></div>
-    )}
-  </div>
-);
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='Wazoo'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '0.5em' : '1em',
+      }}
+    />
+    <Header
+      as='h2'
+      content='You can search for the best restaurants instantly.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.2em' : '1.3em',
+        marginBottom: '30px',
+      }}
+    />
+    <Button size='huge'>
+      <a style={{color:'green'}} color='green' href="/signup">Get Started</a>
+      <Icon style={{color:'green'}} name='right arrow' />
+    </Button>
+  </Container>
+)
 
-HomePage.propTypes = {
-  isAuthenticated: PropTypes.bool.isRequired,
-  logout: PropTypes.func.isRequired
+HomepageHeading.propTypes = {
+  mobile: PropTypes.bool,
 }
 
-function mapStateToProps(state){
-  return{
-    isAuthenticated: !!state.user.token
+class DesktopContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { children } = this.props
+    const { fixed } = this.state
+
+    return (
+      <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment
+            color='green'
+            inverted
+            textAlign='center'
+            style={{ minHeight: 600, padding: '1em 0em' }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>
+                  Home
+                </Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted={!fixed}>
+                  <a style={{color:'white'}} color='green' href="/login">Log in</a>
+                  </Button>
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    <a style={{color:'white'}} href="/signup">Signup</a>
+                  </Button>
+                   
+                  
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <HomepageHeading />
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Responsive>
+    )
   }
 }
 
-export default connect(mapStateToProps, { logout: actions.logout })(HomePage);
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+  state = {}
+
+  handlePusherClick = () => {
+    const { sidebarOpened } = this.state
+
+    if (sidebarOpened) this.setState({ sidebarOpened: false })
+  }
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
+
+  render() {
+    const { children } = this.props
+    const { sidebarOpened } = this.state
+
+    return (
+      <Responsive maxWidth={Responsive.onlyMobile.maxWidth} >
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
+            <Menu.Item as='a' active>
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>Log in</Menu.Item>
+            <Menu.Item as='a'>Sign Up</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher
+            dimmed={sidebarOpened}
+            onClick={this.handlePusherClick}
+            style={{ minHeight: '100vh' }}
+          >
+            <Segment
+              inverted
+              textAlign='center'
+              style={{ minHeight: 350, padding: '1em 0em' }}
+              vertical
+            >
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted>
+                      Log in
+                    </Button>
+                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                      Sign Up
+                    </Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomepageHeading mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Responsive>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const ResponsiveContainer = ({ children }) => (
+  <div>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </div>
+)
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const HomepageLayout = () => (
+  <ResponsiveContainer>
+  </ResponsiveContainer>
+)
+
+export default HomepageLayout
