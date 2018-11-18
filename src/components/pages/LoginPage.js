63905948cee1f84f6239dcc8810fe996854e11fd
John <jjcerpa@uninorte.edu.co>
2018-11-18 15:24:23 -0500

    Update LoginPage.js

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,31 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import LoginForm from "../forms/LoginForm";
-import { login } from "../../actions/auth";
-import { Grid, Image, Header } from 'semantic-ui-react';
+import {login} from "../../actions/auth";
+import {Grid, Image, Header} from 'semantic-ui-react';
 
 class LoginPage extends React.Component {
-  submit = data =>
-    this.props.login(data).then(() => this.props.history.push("/dashboard"));
+    submit = data => this
+        .props
+        .login(data)
+        .then(() => this.props.history.push("/dashboard"));
 
-  render() {
-    return (
-      <Grid textAlign='center' style={{height:'100%', margin:'30px'}} verticalAlign='middle'>
-        <Header as='h2' color='green' textAlign='center'>
-          <Image src='/favicon.ico' /> Log-in now
-          <LoginForm submit={this.submit} />
-        </Header>
-      </Grid>      
-    );
-  }
+    render() {
+        return (
+            <Grid
+                textAlign='center'
+                style={{
+                  height: '100%',
+                  margin: '30px'
+                }}
+                verticalAlign='middle'>
+                <Header as='h2' color='green' textAlign='center'>
+                    <Image src='/favicon.ico'/>
+                    Log-in now
+                    <LoginForm submit={this.submit}/>
+                </Header>
+            </Grid>
+        );
+    }
 }
 
 LoginPage.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired
-  }).isRequired,
-  login: PropTypes.func.isRequired
+    history: PropTypes
+        .shape({push: PropTypes.func.isRequired})
+        .isRequired,
+    login: PropTypes.func.isRequired
 };
 
-export default connect(null, { login })(LoginPage);
+export default connect(null, {login})(LoginPage);
