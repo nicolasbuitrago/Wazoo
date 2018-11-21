28cc3956b1af2c03408cb71fcca08fc57f147a04
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2018-11-20 22:33:16 -0500

    Cambios en Home Page

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react'
 import {
   Button,
   Container,
-  Divider,
   Grid,
   Header,
   Icon,
   Image,
-  Link,
+  List,
   Menu,
   Responsive,
   Segment,
@@ -20,29 +19,30 @@ const HomepageHeading = ({ mobile }) => (
   <Container text>
     <Header
       as='h1'
-      content='Wazoo'
+      
       inverted
       style={{
         fontSize: mobile ? '2em' : '4em',
         fontWeight: 'normal',
         marginBottom: 0,
-        marginTop: mobile ? '0.5em' : '1em',
+        marginTop: mobile ? '1.5em' : '2.2em',
       }}
-    />
+    >
+      <Image circular src='./imgs/wazoo.png' /> Wazoo
+    </Header>
     <Header
       as='h2'
-      content='You can search for the best restaurants instantly.'
+      content='Restaurant?  Wazoo.'
       inverted
       style={{
         fontSize: mobile ? '1.5em' : '1.7em',
         fontWeight: 'normal',
-        marginTop: mobile ? '0.2em' : '1.3em',
-        marginBottom: '30px',
+        marginTop: mobile ? '0.5em' : '1.5em',
       }}
     />
-    <Button size='huge'>
-      <a style={{color:'green'}} color='green' href="/signup">Get Started</a>
-      <Icon style={{color:'green'}} name='right arrow' />
+    <Button color='green' size='huge'>
+      Get Started
+      <Icon name='right arrow' />
     </Button>
   </Container>
 )
@@ -51,6 +51,10 @@ HomepageHeading.propTypes = {
   mobile: PropTypes.bool,
 }
 
+/* Heads up!
+ * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
+ * It can be more complicated, but you can create really flexible markup.
+ */
 class DesktopContainer extends Component {
   state = {}
 
@@ -69,10 +73,9 @@ class DesktopContainer extends Component {
           onBottomPassedReverse={this.hideFixedMenu}
         >
           <Segment
-            color='green'
             inverted
             textAlign='center'
-            style={{ minHeight: 600, padding: '1em 0em' }}
+            style={{ minHeight: 700, padding: '1em 0em' }}
             vertical
           >
             <Menu
@@ -87,14 +90,12 @@ class DesktopContainer extends Component {
                   Home
                 </Menu.Item>
                 <Menu.Item position='right'>
-                  <Button as='Link' inverted={!fixed} to='/login'>
+                  <Button as='a' href='/login' inverted={!fixed} color='green'>
                     Log in
                   </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    <a style={{color:'white'}} href="/signup">Signup</a>
+                  <Button as='a' href='/signup' inverted={!fixed} color='blue' primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    Sign Up
                   </Button>
-                   
-                  
                 </Menu.Item>
               </Container>
             </Menu>
@@ -128,14 +129,12 @@ class MobileContainer extends Component {
     const { sidebarOpened } = this.state
 
     return (
-      <Responsive maxWidth={Responsive.onlyMobile.maxWidth} >
+      <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
         <Sidebar.Pushable>
           <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
             <Menu.Item as='a' active>
               Home
             </Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
           </Sidebar>
 
           <Sidebar.Pusher
@@ -155,10 +154,10 @@ class MobileContainer extends Component {
                     <Icon name='sidebar' />
                   </Menu.Item>
                   <Menu.Item position='right'>
-                    <Button as='a' inverted>
+                    <Button as='a' href='/login' inverted >
                       Log in
                     </Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                    <Button as='a' href='/signup' inverted style={{ marginLeft: '0.5em' }}>
                       Sign Up
                     </Button>
                   </Menu.Item>
@@ -192,7 +191,119 @@ ResponsiveContainer.propTypes = {
 
 const HomepageLayout = () => (
   <ResponsiveContainer>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column width={7}>
+            <Header as='h3' style={{ fontSize: '1.8em' }}>
+              We help you to find the best restaurant.
+            </Header>
+            <p style={{ fontSize: '1.1em' }}>
+            We can show you the information of the restaurants so you can decide which one is the best.
+            If you want to find the perfect restaurant, you just have to create an account in Wazoo. Creating an account is simple.
+            </p>
+            <Header as='h3' style={{ fontSize: '1.8em' }}>
+              Favorite restaurants
+            </Header>
+            <p style={{ fontSize: '1.1em' }}>
+            You can save your favorite restaurants and watch them whenever you want. You can easily 
+            find the information of your favorite restaurants and locate them on the map.
+            </p>
+          </Grid.Column>
+          <Grid.Column floated='right' width={8}>
+            <Image bordered rounded size='huge' src='./imgs/map.png' />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    {/* <Segment style={{ padding: '0em' }} vertical>
+      <Grid celled='internally' columns='equal' stackable>
+        <Grid.Row textAlign='center'>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              "What a Company"
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+          </Grid.Column>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              "I shouldn't have gone with their competitor."
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              <Image avatar src='/images/avatar/large/nan.jpg' />
+              <b>Nan</b> Chief Fun Officer Acme Toys
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Breaking The Grid, Grabs Your Attention
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Instead of focusing on content creation and hard work, we have learned how to master the
+          art of doing nothing by providing massive amounts of whitespace and generic content that
+          can seem massive, monolithic and worth your attention.
+        </p>
+        <Button as='a' size='large'>
+          Read More
+        </Button>
+        <Divider
+          as='h4'
+          className='header'
+          horizontal
+          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+        >
+          <a href='#'>Case Studies</a>
+        </Divider>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Did We Tell You About Our Bananas?
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
+          it's really true. It took years of gene splicing and combinatory DNA research, but our
+          bananas can really dance.
+        </p>
+        <Button as='a' size='large'>
+          I'm Still Quite Interested
+        </Button>
+      </Container>
+    </Segment> */}
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Database</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>Wazoo Plans</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Contributors' />
+              <List link inverted>
+                <List.Item as='a' href='https://github.com/nicolasbuitrago' >Nicolas Buitrago</List.Item>
+                <List.Item as='a' href='https://github.com//WilliamCadenas21' >William Cadenas</List.Item>
+                <List.Item as='a' href='https://github.com/j0hnc' >Jhon Cerpa</List.Item>
+                <List.Item as='a' href='https://github.com/DavidCuentasMar' >David Cuentas</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>
+                Wazoo
+              </Header>
+              <p>
+              The best application to find restaurants.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
   </ResponsiveContainer>
 )
-
 export default HomepageLayout
